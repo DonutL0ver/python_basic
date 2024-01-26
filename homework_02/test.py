b8c37a5222eafa2c2c3be3a81140271daed51976
DonutL0ver <yarullin@lympro.ru>
2024-01-26 10:02:22 +0300

    Homework3_TEST

---
+++ homework_02/test.py
@@ -0,0 +1,6 @@
+from base import Vehicle
+
+vehicle = Vehicle(311, 55, 12)
+vehicle.started = False
+vehicle.fuel = 0
+vehicle.start()
---
+++ homework_03/APP/mai.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+todos = []
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+@app.post("/todos/{todo_item}")
+def create_todo(todo_item: str):
+    todos.append(todo_item)
+    return {"message": "Задача добавлена"}
+
+@app.delete("/todos/{index}")
+def delete_todo(index: int):
+    if index < len(todos):
+        del todos[index]
+        return {"message": "Задача удалена"}
+    else:
+        return {"message": "Неверный индекс"}
+
