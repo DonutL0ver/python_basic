6ab4e2e931fbed800ba29b857a99c5f44db98172
DonutL0ver <yarullin@lympro.ru>
2024-03-09 18:50:07 +0300

    Homework4_11

--- homework_04/main.py
+++ homework_04/main.py
@@ -57,6 +57,6 @@ async def async_main():
 async def on_startup():
     await create_tables()
 
-if __name__ == "__main__":
+if '__name__' == "__main__":
     asyncio.run(async_main())
     uvicorn.run(app, host="127.0.0.1", port=8000)
