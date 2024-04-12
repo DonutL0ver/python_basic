4bae469454467cfa9426ba8948fd5e3e3ad5c351
DonutL0ver <yarullin@lympro.ru>
2024-04-12 11:44:24 +0300

    Homework_04_0.1

---
+++ homework_04/async_module.py
@@ -0,0 +1,21 @@
+from sqlalchemy.util import asyncio
+from homework_04.jsonplaceholder_requests import fetch_users_data, fetch_posts_data
+from homework_04.myenv.DB import create_tables, add_users_to_db, add_posts_to_db
+
+
+async def async_main():
+    await create_tables()
+    users_data, posts_data = await asyncio.gather(
+        fetch_users_data(),
+        fetch_posts_data()
+    )
+    await asyncio.gather(
+        add_users_to_db(users_data),
+        add_posts_to_db(posts_data)
+    )
+
+def main():
+    asyncio.run(async_main())
+
+if __name__ == "__main__":
+    main()
