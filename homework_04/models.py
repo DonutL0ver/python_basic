2dec6531b1b24e6df7d26950242b900e0ff7ee9c
DonutL0ver <yarullin@lympro.ru>
2024-04-19 16:48:25 +0300

    Homework_4.4

--- homework_04/models.py
+++ homework_04/models.py
@@ -26,7 +26,7 @@ class Post(Base):
 
 async_engine = create_async_engine('sqlite+aiosqlite:///database.db', echo=True)
 
-async_session = sessionmaker(
+Session = sessionmaker(
     async_engine,
     expire_on_commit=False,
     class_=AsyncSession
